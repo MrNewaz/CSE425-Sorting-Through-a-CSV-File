40535ab2aa3976ff74b351c2e91a58feda087659
MrNewaz <saifur.saif@northsouth.edu>
2021-08-19 16:47:14 +0600

    init

--- searching.py
+++ searching.py
@@ -2,6 +2,13 @@ import pandas as pd
 
 df = pd.read_csv("heart.csv")
 
-result = df[df['age'] == 63]
+print(
+    'Your options are: [ age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall, output ]')
+
+key = input('What do you want to search it by? : ')
+value = int(input('What is the value? : '))
+
+
+result = df[df[key] == value]
 
 print(result)
